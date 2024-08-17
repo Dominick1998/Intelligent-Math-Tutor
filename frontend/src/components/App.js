0eee86ec48fb18a2cc5dac84309552af70e5ec64
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-08-16 23:03:33 -0500

    Create App.js

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Collaboration from './components/Collaboration';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Intelligent Math Tutor</h1>
+            <Collaboration />
+        </div>
+    );
+}
+
+export default App;
